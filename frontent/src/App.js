eff18300566336388611ccf77d22b6832635f8a6
ganeshponmalar <ganeshponmalar.j@gmail.com>
2024-01-14 17:08:19 +0530

    First Commit

---
+++ frontent/src/App.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+import SignUp from "./pages/SignUp";
+import Header from "./Components/Header";
+
+
+export default function app() {
+
+  return <BrowserRouter>
+  <Header/>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/signIn" element={<SignIn />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/profile" element={<Profile />} />
+      <Route path="/singnUp" element={<SignUp />} />
+      <Route path="" />
+    </Routes>
+  </BrowserRouter>
+
+
+}
+
+
